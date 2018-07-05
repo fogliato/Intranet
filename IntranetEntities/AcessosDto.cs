03cbf0402d71b8ad9b608cffb2f61ce3a5b398fc
fogliato <fogliato.jr@gmail.com>
2018-07-05 20:55:12 -0300

    ultimas alterações

--- IntranetEntities/AcessosDto.cs
+++ IntranetEntities/AcessosDto.cs
@@ -5,8 +5,10 @@ namespace IntranetEntities
     public class AcessosDto
     {
         public int id { get; set; }
+        
         public bool cardapio { get; set; }
         public bool ramais { get; set; }
         public int usuarioId { get; set; }
+
     }
 }
